30a7a676ec47e555f46818df0945473fed2e5905
Victor Augusto Pereira Da Silva <victor.augusto.devtech@gmail.com>
2026-03-04 23:51:16 -0300

    so deixei um pouquinho mais organizado para entender

--- Exercicios_De_Java/CalcularTotalProduto.java
+++ Exercicios_De_Java/CalcularTotalProduto.java
@@ -14,14 +14,17 @@ public class CalcularTotalProduto {
         // Para digitar este linha automaticamente digita shoi e ctrl + espaço
         nomeProduto = JOptionPane.showInputDialog(null,"\nDigite o nome do produto: ");
 
-        valorProduto = Double.parseDouble(JOptionPane.showInputDialog(null,"\nDigite o valor do produto: R$"));
+        valorProduto = Double.parseDouble(
+                JOptionPane.showInputDialog(null,"\nDigite o valor do produto: R$"));
 
-        quantidadeProduto = Integer.parseInt(JOptionPane.showInputDialog(null,"\nDigite a quantidade de produto: "));
+        quantidadeProduto = Integer.parseInt(
+                JOptionPane.showInputDialog(null,"\nDigite a quantidade de produto: "));
 
         totalProduto = valorProduto * quantidadeProduto;
 
 
-        JOptionPane.showMessageDialog(null, "Nome do produto: "+ nomeProduto +
+        JOptionPane.showMessageDialog(null,
+                "Nome do produto: "+ nomeProduto +
                 "\nValor do produto R$"+ valorProduto +
                 "\nQuantidade de produto: "+ quantidadeProduto +
                 "\nValor total dos produtos R$" + totalProduto);
